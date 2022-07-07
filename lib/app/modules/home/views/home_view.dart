54055f826449eb456d0e52f46d92ded665120da6
SirusPlava <aoi.sirus@gmail.com>
2022-07-07 19:20:04 +0700

    Gridviews & Sidebar (3)

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
 
+import '../../../Utils/widgets/sidebar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        backgroundColor: Colors.blue[100],
+        body: Row(children: [
+          Expanded(
+            flex: 2,
+            child: Sidebar(),
+          ),
+          Expanded(
+              flex: 15,
+              child: Container(
+                color: Colors.white,
+              ))
+        ]));
   }
 }
